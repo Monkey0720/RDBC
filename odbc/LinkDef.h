06bfe5598c5b65051c5bb13850cfc74193a44a6c
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-03-08 10:27:36 -0500

    put streamers back

--- odbc/LinkDef.h
+++ odbc/LinkDef.h
@@ -1,11 +1,11 @@
 #ifdef __CINT__
 
-#pragma link C++ class ODBCCallableStatement-!;
-#pragma link C++ class ODBCConnection-!;
-#pragma link C++ class ODBCDatabaseMetaData-!;
-#pragma link C++ class ODBCPreparedStatement-!;
-#pragma link C++ class ODBCResultSet-!;
-#pragma link C++ class ODBCResultSetMetaData-!;
-#pragma link C++ class ODBCStatement-!;
+#pragma link C++ class ODBCCallableStatement+;
+#pragma link C++ class ODBCConnection+;
+#pragma link C++ class ODBCDatabaseMetaData+;
+#pragma link C++ class ODBCPreparedStatement+;
+#pragma link C++ class ODBCResultSet+;
+#pragma link C++ class ODBCResultSetMetaData+;
+#pragma link C++ class ODBCStatement+;
 
 #endif // __CINT__
